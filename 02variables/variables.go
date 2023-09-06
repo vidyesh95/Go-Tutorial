82bccd28cc0adf6cf5b420d82fb941db2bb5aa95
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-07 00:29:45 +0530

    Learning variables part 2

--- 02variables/variables.go
+++ 02variables/variables.go
@@ -6,4 +6,21 @@ func main() {
 	var username string = "vidyesh"
 	fmt.Println(username)
 	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float32 = 255.45544
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	// Default values and some aliases
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
 }
