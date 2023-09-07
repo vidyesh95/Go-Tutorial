d2aa2970ed3506e6958ee33d39a4bf0aa3d00e79
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-07 14:50:54 +0530

    update go to 1.21.1

---
+++ 06loops/loops.go
@@ -0,0 +1,8 @@
+package main
+
+// Imported format package for formatting text on the screen.
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/vidyesh95/Go-Tutorial
 
-go 1.21.0
+go 1.21.1
