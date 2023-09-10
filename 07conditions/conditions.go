4eb6eca71a1b3ebce15a2d871c9bfc8b89ae051d
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-10 11:51:44 +0530

    Condition in Go

--- 07conditions/conditions.go
+++ 07conditions/conditions.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// Conditons in Go
 func main() {
 	fmt.Println("Conditions in Go")
+	var age int = 18
+	if age >= 18 {
+		fmt.Println("You can vote!")
+	} else {
+		fmt.Println("You cannot vote!")
+	}
 }
