13627e75b539828b4eed82b564aaf206798cb101
Vidyesh Churi <vidyesh95@gmail.com>
2023-10-02 10:29:45 +0530

    Functions in Go

--- 03functions/functions.go
+++ 03functions/functions.go
@@ -37,4 +37,11 @@ func main() {
 	fmt.Println(a, b)
 
 	fmt.Println(split(97))
+
+	// Defer statement
+	// A defer statement defers the execution of a function until the surrounding function returns.
+	// The deferred call's arguments are evaluated immediately, but the function call is not executed until the surrounding function returns.
+	// Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order.
+	defer fmt.Println("deferred statement")
+	fmt.Println("statement after deferred statement")
 }
