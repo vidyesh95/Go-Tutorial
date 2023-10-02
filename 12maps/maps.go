537b6fea39a23b0f629afb732b21a02177072ed3
Vidyesh Churi <vidyesh95@gmail.com>
2023-10-03 00:36:26 +0530

    Maps in Go

--- 12maps/maps.go
+++ 12maps/maps.go
@@ -5,6 +5,13 @@ import (
 	"maps"
 )
 
+/**
+* Maps
+* A map maps keys to values.
+* The zero value of a map is nil.
+* A nil map has no keys, nor can keys be added.
+* The make function returns a map of the given type, initialized and ready for use.
+ */
 func main() {
 	map1 := make(map[string]int)
 	map1["key1"] = 7
