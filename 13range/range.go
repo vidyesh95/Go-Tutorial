0a3c561d5b68e3543cb8406401fe5a0bbf8852e3
Vidyesh Churi <vidyesh95@gmail.com>
2023-10-03 20:58:42 +0530

    Range in Go

--- 13range/range.go
+++ 13range/range.go
@@ -26,4 +26,12 @@ import "fmt"
  */
 func main() {
 	fmt.Println("range")
+
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		fmt.Println("num:", num)
+		sum += num
+	}
+	fmt.Println("sum:", sum)
 }
